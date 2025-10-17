20d5f288c026130c25afad07d505705428fb0251
Hamza Khan <hamzakhan211200@gmail.com>
2025-10-17 10:51:10 +0200

    apis updated
    user apis adedd update,delete and change password

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
+from commons.utils.authUtil import jwt_required
 from . import services
 
 # Create your views here.
@@ -13,3 +14,21 @@ def login(request):
 @require_http_methods(["POST"])
 def create(request):
     return services.create_user_service(request)
+
+@jwt_required
+@csrf_exempt
+@require_http_methods(["PUT"])
+def change_password(request):
+    return services.change_user_password(request)
+
+@jwt_required
+@csrf_exempt
+@require_http_methods(["PUT"])
+def update(request):
+    return services.update_user(request)
+
+@jwt_required
+@csrf_exempt
+@require_http_methods(["DELETE"])
+def delete(request,user_id):
+    return services.delete_user(request,user_id)
